f0bb811c3359de31c81145e6f975f00bd22a7d79
Abhishek Panda <panda.abhis@gmail.com>
2019-10-24 02:42:59 +0530

    Game setup

---
+++ main.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<SDL2/SDL.h>
+
+int main(int argc, char *argv[]) {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow("Game Programming",
+                                SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED,
+                                640,
+                                480,
+                                0
+                                );
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255,255,255, 255);
+
+    SDL_Rect rect = {220, 140, 200, 200};
+
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(window);
+    SDL_Quit();
+}
