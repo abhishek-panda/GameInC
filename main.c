3c8a69cde9dec2be81fb9d8dd991c51568b7d71c
Abhishek Panda <panda.abhis@gmail.com>
2019-11-07 02:14:47 +0530

    changes

--- main.c
+++ main.c
@@ -1,9 +1,80 @@
 #include<stdio.h>
 #include<SDL2/SDL.h>
 
+typedef struct
+{
+   int x,y;
+   short life;
+   char *name;
+} Man;
+
+int processEvents(SDL_Window *window, Man *m) {
+    SDL_Event event;
+    int done = 0;
+
+    while(SDL_PollEvent(&event)) {
+        switch (event.type)
+        {
+        case SDL_WINDOWEVENT_CLOSE:
+            if(window) {
+                SDL_DestroyWindow(window);
+                window = NULL;
+            }
+            break;
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_ESCAPE:
+                done=1;
+                break;
+            case SDLK_LEFT:
+                m->x -= 10;
+                break;
+            case SDLK_RIGHT:
+                m->x += 10;
+                break;
+            case SDLK_DOWN:
+                m->y += 10;
+                break;
+            case SDLK_UP:
+                m->y -= 10;
+                break;
+            default:
+                break;
+            }
+            break;
+        case SDL_QUIT:
+            done=1;
+            break;
+        default:
+            break;
+        }
+    }
+
+    const Uint8 *state = SDL_GetKeyboardState(NULL)
+
+    return done;
+}
+
+void doRender(SDL_Renderer *renderer, Man *m) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_Rect rect = { m->x, m->y, 200, 200 };
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+}
+
 int main(int argc, char *argv[]) {
     SDL_Window *window;
     SDL_Renderer *renderer;
+    Man m;
+    m.x = 220;
+    m.y = 140;
 
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -16,17 +87,18 @@ int main(int argc, char *argv[]) {
                                 );
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255,255,255, 255);
+    //The window is open: enter the event loop
+    int done = 0;
+    
 
-    SDL_Rect rect = {220, 140, 200, 200};
+    while (!done) {
+        //Checks for events
+        done = processEvents(window, &m);
 
-    SDL_RenderFillRect(renderer, &rect);
+        doRender(renderer, &m);
 
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(5000);
+        SDL_Delay(100);
+    }
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(window);
